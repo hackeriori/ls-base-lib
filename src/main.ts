a8c2e6ed5b0fba9324b39d26e85b93e9112bbe6a
Administrator <453843655@qq.com>
2024-11-06 17:03:11 +0800

    移除多余css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import router from "./router";
+import 'element-plus/dist/index.css';
+import '../public/css/global.css';
+import '../public/css/ls.css'
 
 import App from './App.vue'
 
